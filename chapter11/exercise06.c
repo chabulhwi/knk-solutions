2b2f669aaed69da3e5f23ed377dfecf2d94db704
Bulhwi Cha <chabulhwi@semmalgil.com>
2025-07-03 18:20:57 +0900

    fix: second-largest can be the same as largest
    When the largest element of an array occurs more than one time, the
    second-largest one should be the same as the largest.

--- chapter11/exercise06.c
+++ chapter11/exercise06.c
@@ -24,24 +24,14 @@ int main(void)
 	find_two_largest(n, a, &largest, &second_largest);
 
 	printf("Largest: %d\n", largest);
-	printf("Second-largest: ");
-
-	for (int i = 0; i < n; i++) {
-		if (a[i] < largest)
-			all_the_same = false;
-	}
-
-	if (all_the_same)
-		printf("none\n");
-	else
-		printf("%d\n", second_largest);
+	printf("Second-largest: %d\n", second_largest);
 
 	return 0;
 }
 
 void find_two_largest(int n, int a[n], int *largest, int *second_largest)
 {
-	bool all_the_same = true;
+	int largest_count = 0;
 
 	*largest = a[0];
 	for (int i = 1; i < n; i++) {
@@ -50,18 +40,24 @@ void find_two_largest(int n, int a[n], int *largest, int *second_largest)
 	}
 
 	for (int i = 0; i < n; i++) {
+		if (a[i] == *largest)
+			largest_count++;
+	}
+
+	if (largest_count > 1) {
+		*second_largest = *largest;
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
 		if (a[i] < *largest) {
-			all_the_same = false;
 			*second_largest = a[i];
+			break;
 		}
 	}
 
-	if (all_the_same) {
-		return;
-	} else {
-		for (int i = 0; i < n; i++) {
-			if (*second_largest < a[i] && a[i] < *largest)
-				*second_largest = a[i];
-		}
+	for (int i = 0; i < n; i++) {
+		if (*second_largest < a[i] && a[i] < *largest)
+			*second_largest = a[i];
 	}
 }
