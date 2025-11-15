75b87ce81dc2b780463663936c8548db008a5951
Bulhwi Cha <chabulhwi@semmalgil.com>
2025-11-15 18:28:08 +0900

    fix: format C source files except for a few

--- scripts/indent.sh
+++ scripts/indent.sh
@@ -1,4 +1,13 @@
-find . -name "*.c" -o -name '*.h' -exec indent \
-     -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce \
-     -cli0 -d0 -di1 -nfc1 -i8 -ip0 -l80 -lp -npcs -nprs -npsl -sai \
-     -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il0 {} +
+find . \( -name '*.c' -o -name '*.h' \) \
+    ! \( -path "./chapter16/*" \
+    -name 'exercise02d.c' -o \
+    -name 'exercise03d.c' -o \
+    -name 'exercise04c.c' -o \
+    -name 'exercise04d.c' -o \
+    -name 'exercise08b.h' -o \
+    -name 'exercise10*.c' -o \
+    -name 'exercise14b.c' \) \
+    -exec indent \
+    -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce \
+    -cli0 -d0 -di1 -nfc1 -i8 -ip0 -l80 -lp -npcs -nprs -npsl -sai \
+    -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il0 {} +
