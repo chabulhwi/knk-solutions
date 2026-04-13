678a918392813b230e01622210b96d5d63e06841
Bulhwi Cha <chabulhwi@semmalgil.com>
2026-04-13 17:24:56 +0900

    refactor: rename `day_str` array
    Change the name of the array `day_str` to `month_day_str`.

--- chapter13/project02c.c
+++ chapter13/project02c.c
@@ -11,7 +11,7 @@ int read_line(char str[], int n);
 int main(void)
 {
 	char reminders[MAX_REMIND][MSG_LEN + 3];
-	char day_str[6], msg_str[MSG_LEN + 1];
+	char month_day_str[6], msg_str[MSG_LEN + 1];
 	int month, day, i, j, num_remind = 0;
 
 	for (;;) {
@@ -39,17 +39,17 @@ int main(void)
 			while (getchar() != '\n')
 				/* empty loop body */ ;
 		} else {
-			sprintf(day_str, "%2d/%2.2d", month, day);
+			sprintf(month_day_str, "%2d/%2.2d", month, day);
 			read_line(msg_str, MSG_LEN);
 
 			for (i = 0; i < num_remind; i++) {
-				if (strcmp(day_str, reminders[i]) < 0)
+				if (strcmp(month_day_str, reminders[i]) < 0)
 					break;
 			}
 			for (j = num_remind; j > i; j--)
 				strcpy(reminders[j], reminders[j - 1]);
 
-			strcpy(reminders[i], day_str);
+			strcpy(reminders[i], month_day_str);
 			strcat(reminders[i], msg_str);
 
 			num_remind++;
