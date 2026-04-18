65a824c2c7a895b6c5a81c181c37d09bac1fcefa
Bulhwi Cha <chabulhwi@semmalgil.com>
2026-04-18 19:09:23 +0900

    refactor: remove unnecessary space character

--- chapter04/project06.c
+++ chapter04/project06.c
@@ -8,7 +8,7 @@ int main(void)
 		second_sum, total;
 
 	printf("Enter the first 12 digits of a EAN: ");
-	scanf("%1d%1d%1d%1d%1d%1d %1d%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5,
+	scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5,
 	      &i6, &i7, &i8, &i9, &i10, &i11, &i12);
 
 	first_sum = i2 + i4 + i6 + i8 + i10 + i12;
