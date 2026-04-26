1d93f9968395864dd489d917d3b065e8a42bb1b3
Bulhwi Cha <chabulhwi@semmalgil.com>
2026-04-26 11:42:19 +0900

    fix: check whether message is empty

--- chapter13/project17.c
+++ chapter13/project17.c
@@ -5,21 +5,36 @@
 
 #define MAX_LEN 2000
 
+bool is_empty(const char *message);
 bool is_palindrome(const char *message);
 
 int main(void)
 {
 	char message[MAX_LEN + 2];
+	size_t len = 0;
 
 	printf("Enter a message: ");
 	fgets(message, MAX_LEN + 2, stdin);
+	len = strlen(message);
 
-	if (is_palindrome(message))
+	if (len == 0 || is_empty(message))
+		printf("Empty message\n");
+	else if (len == 1 || is_palindrome(message))
 		printf("Palindrome\n");
 	else
 		printf("Not a palindrome\n");
 
 	return 0;
+}
+
+bool is_empty(const char *message)
+{
+	for (const char *p = message; *p != '\0'; p++) {
+		if (!isspace(*p))
+			return false;
+	}
+
+	return true;
 }
 
 bool is_palindrome(const char *message)
